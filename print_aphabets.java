c23d0d26e2f8a157da132b439d9a647771e23eb3
saurabhk0727 <saurabhkarale0727@gmail.com>
2024-05-28 11:53:55 +0530

    SK 28/05

---
+++ print_aphabets.java
@@ -0,0 +1,13 @@
+package Java_Project;
+public class print_aphabets{
+
+    public static void main(String args []){
+
+        for(int i=0; i<26; i++){
+            char c= (char) ('a'+i);
+        System.out.print(c+" ");
+        }
+        
+    }
+    
+}
